a98ccdff8a1e6a729f9ced14b1c6a8d2c2665162
Nicolas Musset <musset.nicolas@gmail.com>
2022-09-06 08:07:35 +0200

    Add script for running a single documentation sample with shards

---
+++ scripts/run-sample.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+opt_short="d"
+opt_long="debug"
+
+OPTS=$(getopt -o "$opt_short" -l "$opt_long" -- "$@")
+
+eval set -- "$OPTS"
+
+# default values
+build_type=Release
+
+# get args
+while true
+do
+    case "$1" in
+        -d|--debug)
+            build_type=Debug
+            shift;;
+        --) # End of input reading
+            shift; break ;;
+    esac
+done
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ $build_type == "Debug" ];
+then
+    echo "Will run the sample using the debug version of shards"
+    shards_exe="shardsd.exe"
+else
+    echo "Will run the sample using the release version of shards"
+    shards_exe="shardsr.exe"
+    # required on Windows for release build
+    . $script_dir/env.sh
+fi
+
+# execute commands
+$script_dir/build/$shards_exe $script_dir/docs/samples/run-sample.edn --file $*
